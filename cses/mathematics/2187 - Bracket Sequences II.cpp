185d427697eee055119a72eb52302415cf1be398
Jashkayth <120001720+Jashkayth@users.noreply.github.com>
2026-09-11 02:31:33 +0530

    Add Bracket Sequences II (mathematics)

---
+++ cses/mathematics/2187 - Bracket Sequences II.cpp
@@ -0,0 +1,95 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<unordered_map>
+#include<unordered_set>
+#include<map>
+#include<iomanip>
+#include<stack>
+#include<math.h>
+#include<set>
+#include<queue>
+#include<climits>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define int long long
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+const int MOD=1e9+7;
+const int MAXN=1e6+1;
+int fact[MAXN];
+int inv_fact[MAXN];
+int power(int x,int y)
+{
+    if(y==0)
+    {
+        return 1;
+    }
+    int z=power((x*x)%MOD,y/2)%MOD;
+    if(y%2==0)
+    {
+        return z%MOD;
+    }
+    else
+    {
+        return (z*x)%MOD;
+    }
+}
+void factorial()
+{
+    fact[0]=1;
+    for(int i=1;i<MAXN;i++)
+    {
+        fact[i]=i*fact[i-1];
+        fact[i]%=MOD;
+    }
+}
+void inv_factorial()
+{
+    inv_fact[MAXN-1]=power(fact[MAXN-1],MOD-2);
+    for(int i=MAXN-2;i>=0;i--)
+    {
+        inv_fact[i]=inv_fact[i+1]*(i+1);
+        inv_fact[i]%=MOD;
+    }
+}
+void fill()
+{
+    factorial();
+    inv_factorial();
+}
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int tot_open=0;
+    int tot_close=0;
+    fill();
+    for(auto it:s)
+    {
+        if(it=='(')
+        {
+            tot_open++;
+        }
+        else
+        {
+            tot_close++;
+        }
+        if(tot_close>tot_open || (n%2) || (tot_open>n/2))
+        {
+            cout<<0;
+            return 0;
+        }
+    }
+    n=n/2;
+    int rem_open=n-tot_open;
+    int rem_close=n-tot_close;
+    int total=((fact[rem_open+rem_close]*(inv_fact[rem_open]%MOD))%MOD)*(inv_fact[rem_close]%MOD)%MOD;
+    int invalid=((fact[rem_open+rem_close]*(inv_fact[rem_open-1]%MOD))%MOD)*(inv_fact[rem_close+1]%MOD)%MOD;
+    cout<<(total-invalid+MOD)%MOD;
+}
